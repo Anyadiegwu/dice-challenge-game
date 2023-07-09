7c3a21e4ad038a0ef21f4bff0269b92d0bd02fd9
Anyadiegwu <victoranyadiegwu610@gmail.com>
2023-07-09 22:25:17 +0100

    Dicechallenge

---
+++ index.js
@@ -0,0 +1,18 @@
+const rbn = Math.floor(Math.random() * 6) + 1;
+const throw1 = "images/dice" + rbn + ".png";
+const first = document.querySelectorAll("img")[0];
+first.setAttribute("src", throw1);
+
+
+const rdn = Math.floor(Math.random() * 6) + 1;
+const throw2 =  "images/dice" + rdn + ".png";
+const second = document.querySelectorAll("img")[1];
+second.setAttribute("src", throw2);
+
+if(rbn === rdn){
+        document.querySelector("h1").innerHTML = "Draw"
+    }else if(rbn > rdn){
+        document.querySelector("h1").innerHTML = "Player 1 Wins"
+    }else if(rbn < rdn){
+        document.querySelector("h1").innerHTML = "Player 2 Wins"
+    }
